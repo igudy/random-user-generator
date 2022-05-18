92cb836efce145b2cdb29c60330366e07c3247d2
Goodness Igunma <Goodnessigunma1@gmail.com>
2022-05-18 01:01:23 +0100

    Added the random user generator API

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const app = Vue.createApp({
     data(){
         return {
-            firstName: 'Goodness',
-            lastName: 'Igunma',
+            firstName: 'Charles',
+            lastName: 'Divine',
             email: 'Goodnessigunma1@gmail.com',
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
     },
     methods: {
-        getUser(){
-            this.firstName = 'Grace',
-            this.lastName = 'John',
-            this.email = 'Gracejohn@gmail.com',
-            this.gender = 'female',
-            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg',
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            // console.log(results)
+
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
         }
     }
 })
